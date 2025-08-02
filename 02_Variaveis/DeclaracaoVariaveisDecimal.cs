263f153ce882384fd3a7a26b63ec6c71bac5c8fd
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-01 22:02:01 -0300

    Declaração MyMoney

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -0,0 +1,10 @@
+using System
+class DeclaracaoVariaveisDecimal
+	{
+		static void Main(string[]args)
+		{
+			//decimal myMoneyError = 300.0;
+			decimal myMoneyCorrect = 300.0m;
+			Console.WriteLine(myMoneyCorrect);
+		}
+	}	
