6c2b53f79471daad80c4ffe0a8a90a9c6db259dc
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-01 21:44:05 -0300

    Declaração de variáveis

---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,28 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	static void Main()
+	{
+		/*
+		Declaracao Variável
+		*/
+		//Inicialização da Variável
+		int a;
+		
+		a = 22;
+		Console.WriteLine(a);
+		
+		//Reatribuição de valor na Variável
+		a = 56;
+		Console.WriteLine(a);
+		//Outras formas de declaração de Variáveis
+		//Declaração e Inicialização a variavel
+		int b= 925;
+		Console.WriteLine(b);
+		
+		int y = 10, x = 20;
+		int z = y + x;
+		Console.WriteLine(z);
+	}		
+}	
