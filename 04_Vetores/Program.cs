6aa425289652eb7147a9cb864f584afe6803e913
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-08 20:42:25 -0300

    Declaração de vetores e foreach

--- 04_Vetores/Program.cs
+++ 04_Vetores/Program.cs
@@ -10,7 +10,37 @@ namespace _04_Vetores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Declaração de variável
+            //Sintaxe: tipo nomeVariavel = valor
+            string nome = "User";
+            Console.WriteLine("User Name");
+
+            //Declaração de vetores
+            //Sintaxe: tipo[] nomeVariavel = {valor1,valor2,valor3};
+            string[] nomes = { "UserUm", "UserDois", "UserTrês","João", "José", "Maria" };
+            Console.WriteLine(nomes[0]);
+            Console.WriteLine(nomes[1]);
+            Console.WriteLine(nomes[2]);
+
+            //Loop FOR
+            //Sintaxe: for( indice; controle; incrimento)
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("{0}° Nome: {1}",i+1, nomes[i]); 
+            }
+
+            //Impressão dos 100 primeiros números
+            for (int i = 0; i <= 100; i+=2)
+            {
+                Console.WriteLine("Número: {0}", i);
+            }
+
+            // Loop foreach
+            // Sintaxe: foreach( variavel in vetor )
+            foreach (string varName in nomes) 
+            {
+                Console.WriteLine("Nome: {0}", varName)
+            }
         }
     }
 }
