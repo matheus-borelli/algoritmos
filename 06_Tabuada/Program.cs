6278b027e2a87eaec3f0a360d558cfb0b20db7b4
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-08 22:05:18 -0300

    Tabuada funcional

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de 1 a 10: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 1 || numero <= 10)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Número inválido!");
+        }
+    }
+}
