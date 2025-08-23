fc8b5128167ffc4db4c8d656c77847b190f08932
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-22 21:55:23 -0300

    Matriz e Calendario

---
+++ 09_Calendario/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _09_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o mês (1..12):");
+            int mes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano:");
+            int ano = int.Parse(Console.ReadLine());
+
+            //Descobre a quantidade de dias de um mês
+            int diasDoMes = DateTime.DaysInMonth(ano, mes);
+
+            //Descobre o dia da semana do primeiro dia do mês
+            // 0 = Domingo - 6 = Sábado
+            DateTime primeiroDiaMes = new DateTime(ano, mes, 1);
+            int diaSemanaInicio = (int)primeiroDiaMes.DayOfWeek;
+
+            //Matriz de 6 semanas e 7 dias
+            int[,] calendario = new int[6, 7];
+
+            int dia = 1;
+
+            //Preenche a matriz com dias do mês
+            for (int semana = 0; semana < 6; semana++)   // <<< só corrigi aqui
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++)
+                {
+                    if (semana == 0 && diaSemana < diaSemanaInicio)
+                    {
+                        calendario[semana, diaSemana] = 0;
+                    }
+                    else if (dia <= diasDoMes)
+                    {
+                        calendario[semana, diaSemana] = dia;
+                        dia++;
+                    }
+                }
+            }
+
+            // título do calendário
+            Console.WriteLine($"\nCalendário de {primeiroDiaMes.ToString("MMMM")} de {ano}");
+            Console.WriteLine("DOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB");
+
+            //impressão de calendário
+            for (int semana = 0; semana < 6; semana++)
+            {
+                for (int diaSemana = 0; diaSemana < 7; diaSemana++)
+                {
+                    if (calendario[semana, diaSemana] == 0)
+                        Console.Write("\t"); 
+                    else
+                        Console.Write(calendario[semana, diaSemana].ToString("00") + "\t"); // 00 a 09!!!
+                }
+                Console.WriteLine();
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
