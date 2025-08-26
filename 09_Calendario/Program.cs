9fb3a36121a21f75f3f0e16320bf5107c790cfc1
Matheus Yuri <matheussenac2026@gmail.com>
2025-08-25 21:08:23 -0300

    Calendario com inclusão de feriados mensais

--- 09_Calendario/Program.cs
+++ 09_Calendario/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace _09_Calendario
 {
@@ -26,7 +28,7 @@ namespace _09_Calendario
             int dia = 1;
 
             //Preenche a matriz com dias do mês
-            for (int semana = 0; semana < 6; semana++)   // <<< só corrigi aqui
+            for (int semana = 0; semana < 6; semana++)
             {
                 for (int diaSemana = 0; diaSemana < 7; diaSemana++)
                 {
@@ -46,21 +48,86 @@ namespace _09_Calendario
             Console.WriteLine($"\nCalendário de {primeiroDiaMes.ToString("MMMM")} de {ano}");
             Console.WriteLine("DOM\tSEG\tTER\tQUA\tQUI\tSEX\tSAB");
 
+            //int[] diasFeriados = { };
+            int[] diasFeriados = RetornaFeriados(mes, ano);
             //impressão de calendário
             for (int semana = 0; semana < 6; semana++)
             {
                 for (int diaSemana = 0; diaSemana < 7; diaSemana++)
                 {
                     if (calendario[semana, diaSemana] == 0)
-                        Console.Write("\t"); 
+                    {
+                        Console.Write("\t");
+                    }
                     else
-                        Console.Write(calendario[semana, diaSemana].ToString("00") + "\t"); // 00 a 09!!!
+                    {
+                        if (diasFeriados.Contains(calendario[semana, diaSemana]) || diaSemana == 0)
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else
+                            Console.ResetColor();
+
+                        Console.Write(calendario[semana, diaSemana].ToString("00") + "\t");
+                    }
                 }
                 Console.WriteLine();
             }
+            Console.Write("\nFeriados ");
+            for (int i = 0; i < diasFeriados.Length; i++)
+            {
+                if (diasFeriados[i] > 0)
+                {
+                    Console.Write($"{diasFeriados[i].ToString("00")}\t");
+                }
+            }
 
-            
+            Console.ResetColor();
             Console.ReadLine();
+
+        }
+        public static int[] RetornaFeriados(int mes, int ano)
+        {
+            int[] feriados = new int[15];
+
+            int indice = 0;
+            // feriados[indice++] = 11;
+            // feriados[indice++] = 21;
+            if (mes == 1)
+            {
+                feriados[indice++] = 1; //Confraternização Universal
+            }
+            else if (mes == 4)
+            {
+                feriados[indice++] = 21; //Tiradentes
+            }
+            else if (mes == 5)
+            {
+                feriados[indice++] = 1; //Dia do Trabalho
+            }
+            else if (mes == 7)
+            {
+                feriados[indice++] = 9;
+            }
+
+            else if (mes == 9)
+            {
+                feriados[indice++] = 7;
+            }
+            else if (mes == 10)
+            {
+                feriados[indice++] = 12;
+            }
+            else if (mes == 11)
+            {
+                feriados[indice++] = 2;
+                feriados[indice++] = 15;
+                feriados[indice++] = 20;
+            }
+            else if (mes == 12)
+            {
+                feriados[indice++] = 25;
+            }
+            return feriados;
         }
     }
 }
+
